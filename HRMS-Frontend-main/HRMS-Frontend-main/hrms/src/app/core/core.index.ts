3125a26a1702991258287476f820ad97957a3f6a
gopalampavan555 <73430263+gopalampavan555@users.noreply.github.com>
2023-11-23 11:42:18 +0530

    Delete HRMS-Frontend-main/HRMS-Frontend-main directory

--- HRMS-Frontend-main/HRMS-Frontend-main/hrms/src/app/core/core.index.ts
+++
@@ -1,4 +0,0 @@
-export * from './services/data/data.service';
-export * from './helpers/routes/routes';
-export * from './services/side-bar/side-bar.service';
-export * from './services/auth/auth.service';
